cc2cad2dcd855fe71a1489a4cf744774c5da699c
Fluidize <mbxiao1@gmail.com>
2023-05-09 19:09:31 -0500

    better again

--- prime_func.py
+++ prime_func.py
@@ -19,17 +19,14 @@ import tqdm
 
 def gen_primes(count):
     arr = [x for x in range(count+1)]; arr[0:2] = []
-    remove_arr = []
+    print(len(arr))
     iteration = 0;
     while True:
         divisor = arr[iteration]; count = 0
         for num in tqdm.tqdm(arr):
             if not (num % divisor) and (not (num == divisor)):
-                remove_arr.append(num)
-                count += 1
-        for num in arr:
-            if num in remove_arr:
                 arr.remove(num)
+                count += 1
         if count == 0:
             break
         iteration += 1
@@ -38,4 +35,4 @@ def gen_primes(count):
 
 
 if __name__ == "__main__":
-    print(gen_primes(10000))
+    print(len(gen_primes(100000)))
